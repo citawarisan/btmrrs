8c1a110792098ff1b8c6df96f57e4dabf7beb576
Gary <37288203+garylkz@users.noreply.github.com>
2023-06-03 03:11:07 +0800

    Added signup

---
+++ src/main/java/com/citawarisan/btmrrs/SignupServlet.java
@@ -0,0 +1,17 @@
+package com.citawarisan.btmrrs;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/signup")
+public class SignupServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Forward the request to the signup.jsp file
+        request.getRequestDispatcher("/auth/signup.jsp").forward(request, response);
+    }
+}
