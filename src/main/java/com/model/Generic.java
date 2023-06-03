4d4f05b89df91f37199672fe53bf2f343f5a4b1a
Gary <37288203+garylkz@users.noreply.github.com>
2023-06-03 15:10:55 +0800

    Added what's missing

---
+++ src/main/java/com/model/Generic.java
@@ -0,0 +1,11 @@
+package com.model;
+
+public class Generic {
+    private String tableName;
+    private String[] columnName;
+
+    public Generic(String tableName, String[] columnName) {
+        this.tableName = tableName;
+        this.columnName = columnName;
+    }
+}
