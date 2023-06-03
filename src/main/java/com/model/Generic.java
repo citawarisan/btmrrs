185e80e72ce682246919451c1ef3279cafe09dbb
Gary <37288203+garylkz@users.noreply.github.com>
2023-06-03 15:13:50 +0800

    Updated Generic.java

--- src/main/java/com/model/Generic.java
+++ src/main/java/com/model/Generic.java
@@ -1,11 +1,19 @@
 package com.model;
 
 public class Generic {
-    private String tableName;
-    private String[] columnName;
+    final private String tableName;
+    final private String[] columnName;
 
     public Generic(String tableName, String[] columnName) {
         this.tableName = tableName;
         this.columnName = columnName;
+    }
+
+    public String getTableName() {
+        return tableName;
+    }
+
+    public String[] getColumnName() {
+        return columnName;
     }
 }
